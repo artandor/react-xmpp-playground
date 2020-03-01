ee142ab8dbe532b40345cebee6543078de910d81
Nicolas MYLLE <millt.nico@hotmail.fr>
2020-03-01 01:26:05 +0100

    Updates

---
+++ precache-manifest.648e10eba47d72bd66d6e7e95fb16f12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713fe85100abcdee8e2934eeb0c1b6c8",
+    "url": "/react-xmpp-playground/index.html"
+  },
+  {
+    "revision": "73ad20d857aed46b0270",
+    "url": "/react-xmpp-playground/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e46fd9e8566760ca59d0",
+    "url": "/react-xmpp-playground/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "73ad20d857aed46b0270",
+    "url": "/react-xmpp-playground/static/js/2.1d494a88.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-xmpp-playground/static/js/2.1d494a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46fd9e8566760ca59d0",
+    "url": "/react-xmpp-playground/static/js/main.8f69e109.chunk.js"
+  },
+  {
+    "revision": "35f2c2fa478f27e90ddf",
+    "url": "/react-xmpp-playground/static/js/runtime-main.0dd682aa.js"
+  }
+]);
